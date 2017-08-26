449b5a07186492319009ec40d5fe16d9ebfd1671
xiaxiong <740925987@qq.com>
2017-08-26 15:44:27 +0800

    文件上传方法

--- src/main/java/com/zonesion/cloud/web/controller/FileManagementController.java
+++ src/main/java/com/zonesion/cloud/web/controller/FileManagementController.java
@@ -7,6 +7,7 @@ import java.util.Map;
 
 import javax.inject.Inject;
 
+import org.apache.commons.lang3.math.NumberUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -19,6 +20,7 @@ import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import com.zonesion.cloud.service.FileManageMentService;
 import com.zonesion.cloud.service.util.FileUtil;
+import com.zonesion.cloud.web.rest.dto.ResumableInfo;
 
 /**
  * 文件上传controller
@@ -42,7 +44,7 @@ public class FileManagementController {
 	 */
 	@RequestMapping(value = "/image-upload", method = RequestMethod.POST,
             headers = {"content-type=multipart/form-data"})
-	public ResponseEntity<?> uploadFile(MultipartHttpServletRequest request) throws IOException{
+	public ResponseEntity<?> uploadImageFile(MultipartHttpServletRequest request) throws IOException{
 		log.debug("Saving team logo...");
 		String imageUrl = null;
 		Iterator<String> itr = request.getFileNames();
@@ -66,6 +68,35 @@ public class FileManagementController {
             		"</script>";
         	return new ResponseEntity<>(ret, HttpStatus.OK);
         }
+	}
+	
+	/**
+	 * 文件上传
+	 * @param request
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping(value = "/file-upload", method = RequestMethod.POST,
+            headers = {"content-type=multipart/form-data"})
+	public ResponseEntity<?> uploadFile(MultipartHttpServletRequest request) throws Exception{
+		int resumableChunkNumber = NumberUtils.toInt(request.getParameter("flowChunkNumber"), -1);
+		ResumableInfo info = fileManageMentService.getResumableInfo(request);
+		fileManageMentService.saveResumableFile(info, request.getFile("file").getInputStream(), request.getContentLength(), resumableChunkNumber);
+		if (info.checkIfUploadFinished()) {
+			String fileTempUrl = info.getResumableFilePath();
+			String fileName = info.getResumableFilename();
+			String dataFileType = null;
+			int index = fileName.lastIndexOf(".");
+			if(index>-1&&index<fileName.length()-1){
+				dataFileType = fileName.substring(index+1);
+			}
+			// 保存并返回路径
+			String fileUrl = fileManageMentService.saveDataFile(fileTempUrl,
+					FileUtil.LOCAL_UPLOAD_FILE_FOLDER, dataFileType);
+			return new ResponseEntity<>(fileUrl, HttpStatus.OK);
+		}else {
+			return new ResponseEntity<>(null, HttpStatus.OK);
+		}
 	}
 	
 }
