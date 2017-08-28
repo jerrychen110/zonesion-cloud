d4bffe223309fe78f1a33d2a1be516eac0671bf9
cc <jerrychen110@hotmail.com>
2017-08-28 16:59:58 +0800

    home页面和index页面修改，index页面新增js。

--- src/main/webapp/app/home/home.state.js
+++ src/main/webapp/app/home/home.state.js
@@ -68,38 +68,6 @@
                     return $translate.refresh();
                 }]
             }
-        })
-        .state('home-courses', {
-            parent: 'app',
-            url: '/courses/{id}&countUser={countUser}',
-            data: {
-            	authorities: []
-            },
-            views: {
-                'content@': {
-                    templateUrl: 'app/courses/home-courses.html',
-                    controller: 'HomeCoursesController',
-                    controllerAs: 'vm'
-                }
-            },
-            resolve: {
-                translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
-                    $translatePartialLoader.addPart('course');
-                    return $translate.refresh();
-                }],
-                entity: ['$stateParams', 'Course', function($stateParams, Course) {
-                    return Course.get({id : $stateParams.id}).$promise;
-                }],
-                previousState: ["$state", function ($state) {
-                    var currentStateData = {
-                        name: $state.current.name || 'course',
-                        params: $state.params,
-                        url: $state.href($state.current.name, $state.params)
-                    };
-                    return currentStateData;
-                }]
-            }
-
         });
         
     }
