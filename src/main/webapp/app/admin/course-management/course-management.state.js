eda12313d37fb2ddd9914e3905de78c839e0a1e2
xiaxiong <740925987@qq.com>
2017-09-05 15:17:09 +0800

    课程管理-超级管理员。

--- src/main/webapp/app/admin/course-management/course-management.state.js
+++ src/main/webapp/app/admin/course-management/course-management.state.js
@@ -34,39 +34,6 @@
                 }
             }
         })
-        /*
-        .state('course-management.new', {
-            url: '/new',
-            data: {
-                authorities: ['ROLE_ADMIN']
-            },
-            onEnter: ['$stateParams', '$state', '$uibModal', function($stateParams, $state, $uibModal) {
-                $uibModal.open({
-                    templateUrl: 'app/admin/course-management/course-management-dialog.html',
-                    controller: 'CourseManagementDialogController',
-                    controllerAs: 'vm',
-                    backdrop: 'static',
-                    size: 'lg',
-                    resolve: {
-                        entity: function () {
-                            return {
-                                id: null, login: null, name: null, email: null,
-                                activated: true, createdBy: null, createdDate: null,
-                                lastModifiedBy: null, lastModifiedDate: null,
-                                authorities: null
-                            };
-                        }
-                    }
-                }).result.then(function() {
-                    $state.go('course-management', null, { reload: true });
-                }, function() {
-                    $state.go('course-management');
-                });
-            }]
-        })
-        */
-        
-        
         .state('course-management-self-list', {
             parent: 'course-management',
             url: '/course-management-self-list?page&sort&courseType&courseSource',
@@ -75,8 +42,7 @@
                 pageTitle: 'courseManagement.home.title'
             },
             views: {
-//                'courseManagementSub@': {
-                'content@': {
+                'courseManagementSub': {
                     templateUrl: 'app/admin/course-management/course-management-self-list.html',
                     controller: 'CourseManagementSelfListController',
                     controllerAs: 'vm'
@@ -121,7 +87,6 @@
                 }]
             }
         })
-        
         .state('course-management-other-list', {
             parent: 'course-management',
             url: '/course-management-other-list?page&sort&courseType&courseSource',
@@ -130,8 +95,7 @@
                 pageTitle: 'courseManagement.home.title'
             },
             views: {
-//              'courseManagementSub@': {
-                'content@': {
+                'courseManagementSub': {
                     templateUrl: 'app/admin/course-management/course-management-other-list.html',
                     controller: 'CourseManagementOtherListController',
                     controllerAs: 'vm'
@@ -176,9 +140,97 @@
                 }]
             }
         })
+        .state('course-management-create', {
+        	parent: 'course-management',
+            url: '/course-management-create?courseType&courseSource',
+            data: {
+                authorities: ['ROLE_ADMIN']
+            },
+            views: {
+            	'contet@': {
+            		templateUrl: 'app/admin/course-management/course-management-create.html',
+                    controller: 'CourseManagementCreateController',
+                    controllerAs: 'vm'
+            	}
+            },
+            params: {
+            	courseType: {
+                	value: '0',
+                	squash: true
+                },
+                courseSource: {
+                	value: '0',
+                	squash: true
+                }
+            },
+            resolve: {
+                entity: function () {
+                    return {
+                    	id: null, userId: null, title: null, subTitle: null,
+                        status: "0", courseType: null, courseSource: null, lessonNum: 0,
+                        credit: "0", coverPicture: "", introduction: null, goals: null,
+                        recommended: "0", recommendedSort: "0"
+                    };
+                },
+                custParams: ['$stateParams', function ($stateParams){
+                	return {
+                		courseType: $stateParams.courseType,
+                		courseSource: $stateParams.courseSource
+                	};
+                }]
+            }
+        })
+        .state('course-management-edit', {
+        	parent: 'course-management',
+            url: '/{id}/course-management-edit',
+            data: {
+                authorities: ['ROLE_ADMIN']
+            },
+            views: {
+            	'contet@': {
+            		templateUrl: 'app/admin/course-management/course-management-edit.html',
+                    controller: 'CourseManagementEditController',
+                    controllerAs: 'vm'
+            	}
+            },
+            resolve: {
+                entity: ['Course', function (Course) {
+                    return Course.get({id: $stateParams.id});
+                }]
+            }
+        })
         
         
         /*
+        .state('course-management.new', {
+            url: '/new',
+            data: {
+                authorities: ['ROLE_ADMIN']
+            },
+            onEnter: ['$stateParams', '$state', '$uibModal', function($stateParams, $state, $uibModal) {
+                $uibModal.open({
+                    templateUrl: 'app/admin/course-management/course-management-dialog.html',
+                    controller: 'CourseManagementDialogController',
+                    controllerAs: 'vm',
+                    backdrop: 'static',
+                    size: 'lg',
+                    resolve: {
+                        entity: function () {
+                            return {
+                                id: null, login: null, name: null, email: null,
+                                activated: true, createdBy: null, createdDate: null,
+                                lastModifiedBy: null, lastModifiedDate: null,
+                                authorities: null
+                            };
+                        }
+                    }
+                }).result.then(function() {
+                    $state.go('course-management', null, { reload: true });
+                }, function() {
+                    $state.go('course-management');
+                });
+            }]
+        })
         .state('course-management.edit', {
             url: '/{login}/edit',
             data: {
