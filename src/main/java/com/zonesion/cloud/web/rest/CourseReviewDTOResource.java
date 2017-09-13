94b35b63dc9b7a52970cf189b822e2b33f39e478
cc <jerrychen110@hotmail.com>
2017-09-13 17:41:48 +0800

    新增课堂评论添加、更新和删除功能，已完成数据接口，支持restful api调试，并且重新修改dto类。

--- src/main/java/com/zonesion/cloud/web/rest/CourseReviewDTOResource.java
+++ src/main/java/com/zonesion/cloud/web/rest/CourseReviewDTOResource.java
@@ -3,12 +3,16 @@ package com.zonesion.cloud.web.rest;
 import com.codahale.metrics.annotation.Timed;
 import com.zonesion.cloud.service.CourseReviewDTOService;
 import com.zonesion.cloud.service.dto.CourseReviewDTO;
+import com.zonesion.cloud.service.dto.ext.CourseReviewExtDTO;
+import com.zonesion.cloud.web.rest.util.HeaderUtil;
+
 import io.github.jhipster.web.util.ResponseUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -31,9 +35,34 @@ public class CourseReviewDTOResource {
 
     @GetMapping("/coursesReview/{id}")
     @Timed
-    public ResponseEntity<List<CourseReviewDTO>> getCourseReview(@PathVariable Long id) {
+    public ResponseEntity<List<CourseReviewExtDTO>> getCourseReview(@PathVariable Long id) {
         log.debug("REST request to get a page of Courses review");
-        List<CourseReviewDTO> courseReviewDTO = courseReviewDTOService.findCourseReview(id);
-        return ResponseUtil.wrapOrNotFound(Optional.ofNullable(courseReviewDTO));
+        List<CourseReviewExtDTO> courseReviewExtDTO = courseReviewDTOService.findCourseReview(id);
+        return ResponseUtil.wrapOrNotFound(Optional.ofNullable(courseReviewExtDTO));
     }
+    
+    @PostMapping("/coursesReview/")
+    @Timed
+    public CourseReviewDTO saveCourseReview(CourseReviewDTO courseReviewDTO) {
+        log.debug("REST request to get a page of Courses review");
+        return courseReviewDTOService.saveCourseReview(courseReviewDTO);
+        
+    }
+    
+    @PutMapping("/coursesReview/")
+    @Timed
+    public ResponseEntity<Void> updateCourseReview(CourseReviewDTO courseReviewDTO) {
+        log.debug("REST request to update a page of Courses review");
+        courseReviewDTOService.updateCourseReview(courseReviewDTO);
+        return ResponseEntity.ok().headers(HeaderUtil.createEntityDeletionAlert(ENTITY_NAME, courseReviewDTO.getId().toString())).build();
+    } 
+    
+    @DeleteMapping("/coursesReview/{id}")
+    @Timed
+    public ResponseEntity<Void> deleteCourseReview(@PathVariable Long id) {
+        log.debug("REST request to delete Course : {}", id);
+        courseReviewDTOService.deleteCourseReview(id);
+        return ResponseEntity.ok().headers(HeaderUtil.createEntityDeletionAlert(ENTITY_NAME, id.toString())).build();
+    }
+    
 }
