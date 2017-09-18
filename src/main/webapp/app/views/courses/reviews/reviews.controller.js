ca70389377ba37f09cbe8aec7f5915c697e0700f
zenghang <823979470@qq.com>
2017-09-18 11:34:03 +0800

    完善课程评论

--- src/main/webapp/app/views/courses/reviews/reviews.controller.js
+++ src/main/webapp/app/views/courses/reviews/reviews.controller.js
@@ -5,12 +5,26 @@
         .module('zonesionCloudApplicationApp')
         .controller('ReviewsController', ReviewsController);
 
-    ReviewsController.$inject = ['$scope', '$rootScope', '$stateParams', 'CourseService','Principal'];
+    ReviewsController.$inject = ['$scope', '$rootScope', '$stateParams', 'CourseService','Principal','LoginService'];
 
-    function ReviewsController($scope, $rootScope, $stateParams, CourseService,Principal) {
+    function ReviewsController($scope, $rootScope, $stateParams, CourseService,Principal,LoginService) {
         var vm = this;
-        vm.isAuthenticated = Principal.isAuthenticated;
+        vm.isAuthenticated = Principal.isAuthenticated();
+        vm.account =  $rootScope.accountInfo;
+        vm.currentPage = 1;
+        vm.pageSize=10;
+        vm.allreviews = [];
+        vm.openReviewFlag = false;
+        vm.reviewContent = '';
+        vm.openReview = openReview;
+        vm.cancelReview = cancelReview;
+        vm.saveReview = saveReview;
         loadReviews();
+        //登录成功  刷新信息
+        $scope.$on('authenticationSuccess', function() {
+          vm.account =  $rootScope.accountInfo;
+          vm.isAuthenticated = Principal.isAuthenticated();
+        });
 
         function loadReviews() {
         	CourseService.getCourseReviews({
@@ -25,5 +39,38 @@
                 //AlertService.error(error.data.message);
             }
         }
+
+        //打开评论
+        function openReview(){
+          if(!vm.isAuthenticated){
+            LoginService.open();
+          }else{
+            vm.openReviewFlag = true;
+          }
+        }
+
+        //save review
+        function saveReview(){
+          var params = {
+            "content": vm.reviewContent,
+            "courseId": $stateParams.id,
+            "userId": vm.account.id
+          }
+          CourseService.saveReview(params,onSuccess, onError);
+          function onSuccess(data, headers) {
+              vm.allreviews = data;
+              console.log(data);
+          }
+          function onError(error) {
+              //AlertService.error(error.data.message);
+          }
+        }
+
+        //取消评论
+        function cancelReview(){
+          vm.openReviewFlag = false;
+        }
+
+
     }
 })();
