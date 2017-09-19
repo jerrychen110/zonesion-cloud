78660453cf3cf4d2eba6ac78b5c25bdab26bde41
zenghang <823979470@qq.com>
2017-09-19 10:39:56 +0800

    课程笔记的筛选

--- src/main/webapp/app/views/courses/notes/notes.controller.js
+++ src/main/webapp/app/views/courses/notes/notes.controller.js
@@ -9,23 +9,54 @@
 
     function NotesController($scope, $rootScope, $stateParams, CourseService) {
         var vm = this;
+        vm.courseId = $stateParams.id;
         vm.allnotes = [];
-        loadNotes();
+        vm.currentPage = 1;
+        vm.pageSize=1;
+        vm.allreviews = [];
+        vm.selectedLesson={id:null,title:'全部课程'};
+        vm.loadNotes=loadNotes;
+        vm.loadLessons = loadLessons;
+        vm.selectLesson = selectLesson;
+        vm.loadNotes();
+        vm.loadLessons();
         vm.courseLessonId = $stateParams.courseLessonId;
+        //get 笔记数据
         function loadNotes () {
         	CourseService.getCourseNotes({
-        		id: $stateParams.id
+        		id: vm.courseId,
+            page:vm.currentPage,
+            size:vm.pageSize,
+            courseLessonId:vm.selectedLesson.id
             }, onSuccess, onError);
 
             function onSuccess(data, headers) {
-
-                vm.allnotes = data;
-                console.log(data);
+              vm.totalCount = headers('X-Total-Count');
+              vm.allnotes = data;
+              console.log(data);
 
             }
             function onError(error) {
                 //AlertService.error(error.data.message);
             }
+        }
+
+        //get 所有课程
+        function loadLessons(){
+          CourseService.getLessons({id:vm.courseId},onSuccess, onError);
+            function onSuccess(data, headers) {
+                vm.lessonDatas = data;
+                vm.lessonDatas.unshift(vm.selectedLesson);
+            }
+            function onError(error) {
+                //AlertService.error(error.data.message);
+            }
+        }
+
+        //选择课程
+        function selectLesson(lessonData){
+          vm.selectedLesson = {id:lessonData.id,title:lessonData.title};
+          vm.loadNotes();
         }
 
     }
