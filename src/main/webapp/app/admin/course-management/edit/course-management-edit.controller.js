767b569bc4d9b78ea65c9cbefeed20eb340744de
zenghang <823979470@qq.com>
2017-09-21 14:33:15 +0800

    课时新建

--- src/main/webapp/app/admin/course-management/edit/course-management-edit.controller.js
+++ src/main/webapp/app/admin/course-management/edit/course-management-edit.controller.js
@@ -6,10 +6,10 @@
         .controller('CourseManagementEditController', CourseManagementEditController);
 
     CourseManagementEditController.$inject = ['Principal', 'Course', 'entity', 'AlertService', '$state', '$log',
-     '$scope', '$rootScope', 'JhiLanguageService', 'Upload','CourseManagementService','$uibModal'];
+     '$scope', '$rootScope', 'JhiLanguageService', 'Upload','CourseManagementService','$uibModal','EDITOROPTIONS'];
 
     function CourseManagementEditController(Principal, Course, entity, AlertService, $state, $log,
-      $scope, $rootScope, JhiLanguageService, Upload,CourseManagementService,$uibModal) {
+      $scope, $rootScope, JhiLanguageService, Upload,CourseManagementService,$uibModal,EDITOROPTIONS) {
         var vm = this;
 
         vm.authorities = ['ROLE_USER', 'ROLE_ADMIN'];
@@ -26,7 +26,7 @@
           search          : '搜索',
           nothingSelected : '没有选择的专业',
           multipleLabel   : '当前选中数量'
-};
+        };
 
         vm.getOrders = getOrders;
         vm.stateGo = stateGo;
@@ -105,42 +105,7 @@
         };
 
         //富文本插件ckeditor相关设置
-        $scope.editorOptions = {
-	      language: 'zh-cn',
-	      // allowedContent:{
-	      //     $1: {
-	      //         elements: CKEDITOR.dtd,
-	      //         attributes: true,
-	      //         styles: true,
-	      //         classes: true
-	      //     }
-	      // },
-	      image_previewText:' ',
-	      allowedContent: true,
-	      extraPlugins : 'widget,filetools,notification,lineutils,notificationaggregator,uploadwidget,uploadimage',
-	      filebrowserImageUploadUrl: '/api/file/image-upload?type=Images',
-	      filebrowserWindowHeight: '240',
-	      disallowedContent:'script; *[on*];*{*javascript*}',
-	      toolbar :[
-	        { name: 'tools', items: [ 'Maximize'] },
-    		{ name: 'clipboard', items: [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ] },
-    		{ name: 'basicstyles', items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat' ] },
-    		{ name: 'paragraph', items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent',  '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'] },
-    		{ name: 'links', items: [ 'Link', 'Unlink' ] },
-    		{ name: 'insert', items: [ 'Image', 'Table', 'HorizontalRule', 'SpecialChar', 'PageBreak'] },
-    		{ name: 'colors', items: [  'TextColor', 'BGColor', 'Format'] },
-    		{ name: 'styles', items: [ 'Styles', 'Font', 'FontSize' ] },
-	        { name: 'document', items: [ 'Source'] }
-	      ],
-	      on: {
-	        instanceReady: function () {
-	          setTimeout(function () {
-	            $('.cke_reset iframe').contents().find('body').css('background-color','##fff');
-	            $('.cke_reset iframe').contents().find('body').css('margin','0px');
-	          }, 100);
-	        }
-	      }
-	    };
+        $scope.editorOptions = EDITOROPTIONS;
 
         function save () {
             vm.isSaving = true;
