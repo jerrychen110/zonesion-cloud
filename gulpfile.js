68835929d554b91b0548dd9f7b616b137507b5df
xiaxiong <740925987@qq.com>
2017-09-23 09:47:37 +0800

    修改打包后ckeditor报错的处理。

--- gulpfile.js
+++ gulpfile.js
@@ -33,7 +33,7 @@ gulp.task('clean', function () {
     return del([config.dist], { dot: true });
 });
 
-gulp.task('copy', ['copy:i18n', 'copy:languages', 'copy:fonts', 'copy:common', 'copy:swagger', 'copy:images', 'copy:img']);
+gulp.task('copy', ['copy:i18n', 'copy:languages', 'copy:fonts', 'copy:common', 'copy:swagger', 'copy:images', 'copy:img', 'copy:ckeditor']);
 
 gulp.task('copy:i18n', copy.i18n);
 
@@ -48,6 +48,8 @@ gulp.task('copy:swagger', copy.swagger);
 gulp.task('copy:images', copy.images);
 
 gulp.task('copy:img', copy.img);
+
+gulp.task('copy:ckeditor', copy.ckeditor);
 
 /*
 gulp.task('images', function () {
